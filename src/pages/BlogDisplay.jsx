c69b4d5ac8b25d42b7b5cf0e74c00765d9c5f40e
Shivam Kumar Saraffa <ssaraffa786@gmail.com>
2025-03-24 18:45:49 +0530

    blogs fixed

--- src/pages/BlogDisplay.jsx
+++ src/pages/BlogDisplay.jsx
@@ -1,16 +1,30 @@
 import React from 'react';
+import Background from '../assets/bike.jpg';
 
 const BlogDisplay = () => {
   return (
-    <div className="w-full h-[90vh] bg-white relative pt-5 sm:pt-0">
-      <iframe
-        src="https://khetanutkarsh.wordpress.com/blog/?fbclid=PAZXh0bgNhZW0CMTEAAaZ2ozgjDym06QlqN98YVbJDbSLG-kG99j_mbQfTi3EGHOqdl1kGejhdbDY_aem_XQTZ_TlvbIVq2MjY3ffG6Q"
-        title="WordPress Blog"
-        width="100%"
-        height="100%"
-        className="border-none"
-        style={{ minHeight: '100vh' }}
+    <div className="w-full h-[90vh] relative pt-5 sm:pt-0">
+      {/* Blurred Background */}
+      <div
+        className="absolute inset-0 bg-cover bg-center"
+        style={{
+          backgroundImage: `url(${Background})`,
+          filter: 'blur(8px)',
+          zIndex: 1,
+        }}
       />
+
+      {/* Content */}
+      <div className="absolute inset-0 top-36 sm:top-5 flex items-center justify-center z-10">
+        <iframe
+          src="https://khetanutkarsh.wordpress.com/blog/?fbclid=PAZXh0bgNhZW0CMTEAAaZ2ozgjDym06QlqN98YVbJDbSLG-kG99j_mbQfTi3EGHOqdl1kGejhdbDY_aem_XQTZ_TlvbIVq2MjY3ffG6Q"
+          title="WordPress Blog"
+          width="95%"
+          height="100%"
+          className="border-none"
+          style={{ minHeight: '100vh' }}
+        />
+      </div>
     </div>
   );
 };
